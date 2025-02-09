5a77d5ddee2ae0a3f48cc32479e96c02d39f1d3d
mah0504 <maha.amhaouch@umontreal.ca>
2025-02-09 12:53:18 -0500

    1er prog db ;)

---
+++ wesh.sql
@@ -0,0 +1,27 @@
+-- Création de la table 'presences'
+CREATE TABLE presences (
+    nopige INT,
+    nom VARCHAR(50)
+);
+
+-- Création de la table 'cadeaux'
+CREATE TABLE cadeaux (
+    nopige INT,
+    cadeau VARCHAR(50)
+);
+-- Insérer des données dans 'presences'
+INSERT INTO presences (nopige, nom)
+VALUES
+(1, 'Alice'),
+(1, 'Bob');
+
+-- Insérer des données dans 'cadeaux'
+INSERT INTO cadeaux (nopige, cadeau)
+VALUES
+(1, 'iPad'),
+(2, 'Galaxy');
+
+SELECT * 
+FROM cadeaux c
+LEFT OUTER JOIN presences p ON (1=1)
+WHERE c.nopige = p.nopige;
